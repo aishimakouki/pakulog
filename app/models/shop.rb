0a78fc28f21abb1963ad10fbb2dff8faccc9b0d3
kouki <miki.0418.kouki@gmail.com>
2018-06-24 14:02:34 +0900

    バリデーション追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,13 @@
 class Shop < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates : name, presence: true
+  validates : description, presence: true
+  validates : image, presence: true
+  validates : budget_min, presence: true
+  validates : budget_max, presence: true
+  validates : genre_id, presence: true
+  validates : area_id, presence: true
+  validates : user_id, presence: true
+  validates : review_avg, presence: true
+
 end
