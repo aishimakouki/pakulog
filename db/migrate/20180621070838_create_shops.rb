ce8212791d43cce3644740bf7ad54d70612a80f6
kouki <miki.0418.kouki@gmail.com>
2018-06-24 14:56:31 +0900

    migrateにdefalut,null,false追加

--- db/migrate/20180621070838_create_shops.rb
+++ db/migrate/20180621070838_create_shops.rb
@@ -1,15 +1,15 @@
 class CreateShops < ActiveRecord::Migration[5.1]
   def change
     create_table :shops do |t|
-      t.string :name
-      t.text :description
-      t.text :image
-      t.integer :budget_min
-      t.integer :budget_max
-      t.integer :genre_id
-      t.integer :area_id
-      t.integer :user_id
-      t.integer :review_avg
+      t.string :name, null: false, default: ""
+      t.text :description, null: false, default: ""
+      t.text :image,
+      t.integer :budget_min, null: false
+      t.integer :budget_max, null: false
+      t.integer :genre_id, null: false
+      t.integer :area_id, null: false
+      t.integer :user_id, null: false
+      t.integer :review_avg, null: false
 
       t.timestamps
     end
--- db/migrate/20180621071348_create_reviews.rb
+++ db/migrate/20180621071348_create_reviews.rb
@@ -1,12 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
-      t.integer :shop_id
-      t.integer :user_id
-      t.string :title
-      t.text :description
+      t.integer :shop_id, null: false
+      t.integer :user_id, null: false
+      t.string :title, null: false, default: ""
+      t.text :description, null: false, default: ""
       t.text :image
-      t.integer :evaluation
+      t.integer :evaluation, null: false
 
       t.timestamps
     end
