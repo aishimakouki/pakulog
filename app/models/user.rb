901072bbbfe044412d01c8455a36e4d2e4c3e738
kouki <miki.0418.kouki@gmail.com>
2018-06-26 13:33:40 +0900

    shopscontrollerとルーティング作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :shops
   has_many :reviews
 
-  validates : nick_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
