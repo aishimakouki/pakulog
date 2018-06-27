564c8be833791734ec61cd00ba8d278941d1db39
kouki <miki.0418.kouki@gmail.com>
2018-06-27 17:29:51 +0900

    不要削除

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,22 +2,4 @@ class ShopsController < ApplicationController
   def top
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = user.new(user_params)
-    @user.save
-    redirect_to @user
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(
-      :nick_name
-      )
-  end
-
 end
