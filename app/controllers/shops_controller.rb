61b2aee9b7d9a03108a863228fa0dc255ae7262a
kouki <miki.0418.kouki@gmail.com>
2018-06-27 17:21:34 +0900

    deviseのviewinstall

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,23 @@
 class ShopsController < ApplicationController
   def top
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = user.new(user_params)
+    @user.save
+    redirect_to @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :nick_name
+      )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :shops do
-    member do
-    get 'top'
+    collection do
+      get 'top'
     end
   end
+  root "shops#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
