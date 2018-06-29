740eaa7957a965acaf7edb227a7f9b0a63e3c0be
kouki <miki.0418.kouki@gmail.com>
2018-06-29 20:34:56 +0900

    会員登録、ログイン機能、会員情報変更機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :image])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nick_name, :image])
   end
 end
