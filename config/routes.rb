c66f003a0ea740544a878cc141b3771685c31d84
kouki <miki.0418.kouki@gmail.com>
2018-08-04 13:02:27 +0900

    一時的コミット

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     collection do
       get 'top'
     end
+    resources :reviews
   end
   root "shops#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
